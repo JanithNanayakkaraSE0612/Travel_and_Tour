9b40f5920f161ba1d963b18fbebff83dc76be97e
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-12-15 20:07:00 +0530

    Update

--- tourism/src/Components/Navbar/Navbar.jsx
+++ tourism/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './navbar.css'
+import {MdTravelExplore} from 'react-icons/md'
 
 const Navbar = () => {
   return (
-    <div>
-      navbar
-    </div>
+    <section className='navBarSection'>
+        <header className='header flex'>
+            <div className="logoDiv">
+                <a href="#" className='logo flex'>
+                <h1><MdTravelExplore className='icon'/>..Travel.</h1>  
+                </a>
+            </div>
+        </header>
+    </section>
   )
 }
 
