266e7ee4e3f1729e68629fd4e9b02b4e2b78209c
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-12-18 21:17:00 +0530

    update

--- src/components/destinations/Destinations.js
+++ src/components/destinations/Destinations.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import './DestinationsStyles.css'
 
-import BoraBora from '../../assets/borabora.jpg'
-import BoraBora2 from '../../assets/borabora2.jpg'
-import Maldives from '../../assets/maldives.jpg'
-import Maldives2 from '../../assets/maldives2.jpg'
-import KeyWest from '../../assets/keywest.jpg'
+import P1 from '../../assets/P1.jpg'
+import p6 from '../../assets/p6.jpg'
+import p3 from '../../assets/p3.jpg'
+import p4 from '../../assets/p4.png'
+import p5 from '../../assets/p5.png'
+import P2 from '../../assets/p2.jpeg'
 
 function Destinations() {
     return (
         <div name='destinations' className='destinations'>
             <div className="container">
-                <h1>All-Inclusive Resorts</h1>
-                <p>On the Caribbean's Best Beaches</p>
+                <h1>All-Inclusive Place</h1>
+                <p>On the Sri Lanka's Best Places.</p>
                 <div className="img-container">
-                    <img className='span-3 image-grid-row-2' src={BoraBora} alt="/"/>
-                    <img src={BoraBora2} alt="/"/>
-                    <img src={Maldives} alt="/"/>
-                    <img src={Maldives2} alt="/"/>
-                    <img src={KeyWest} alt="/"/>
+                    <img className='span-3 image-grid-row-2' src={P1} alt="/"/>
+                    <img className=' image-grid-row-2' src={p6} alt="/"/>
+                    <img className='span-3 image-grid-row-2' src={p3} alt="/"/>
+                    <img className='span-3 image-grid-row-2'src={p4} alt="/"/>
+                    <img className=' image-grid-row-2' src={P2} alt="/"/>
+                    <img className='span-3 image-grid-row-2'src={p5} alt="/"/>
                 </div>
             </div>
         </div>
