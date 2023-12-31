7226d0dea93a1c28383a8b19b04e9a88d30885cf
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-12-31 16:28:16 +0530

    Update

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -6,6 +6,7 @@ import Video from '../../assets/1217.mp4'
 
 function Hero() {
     return (
+        //github issue
         <div className='hero'>
             <video autoPlay loop muted id='video'>
                 <source src={Video} type='video/mp4' />
